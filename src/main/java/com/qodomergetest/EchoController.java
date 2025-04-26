8342ef5ff05f85b6e5d1682842bf6f4bf5b3e2a0
Sangmin Park <sangmins930@gmail.com>
2025-04-26 15:33:48 +0900

    feat: EchoController 구현

---
+++ src/main/java/com/qodomergetest/EchoController.java
@@ -0,0 +1,18 @@
+package com.qodomergetest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EchoController {
+
+    @GetMapping("/echo")
+    public ResponseEntity<String> get(@RequestBody String request) {
+        return ResponseEntity.ok(request);
+    }
+
+}
