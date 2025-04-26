6669bdb55868c082701fe9bf1e6e92dc757bb5d6
Sangmin Park <sangmins930@gmail.com>
2025-04-26 15:26:47 +0900

    init commit

---
+++ src/main/java/com/qodomergetest/QodoMergeTestApplication.java
@@ -0,0 +1,13 @@
+package com.qodomergetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodoMergeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QodoMergeTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qodomergetest/QodoMergeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qodomergetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QodoMergeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
